80726d4f5ffbff23b7a333def74efbfec33505cd
Bethany Cohen <deskofbethany@gmail.com>
2021-08-21 11:27:34 -0500

    first commit

---
+++ scripts/main.js
@@ -0,0 +1,9 @@
+/*****************************************************************************
+**
+**    Title:      main.js
+**    Project:    
+**    Author:     Bethany Cohen
+**    Contact:    <deskofbethany@gmail.com>
+**    GitHub:     https://github.com/bethco
+**    
+*****************************************************************************/
